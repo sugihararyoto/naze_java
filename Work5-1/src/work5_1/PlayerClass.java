1cc26ff67e6bf18467ce587a834dfb9b1ecedd37
sugihara ryoto <sugihara ryoto>
2023-07-05 13:08:25 +0900

    5-1を行いました。

---
+++ Work5-1/src/work5_1/PlayerClass.java
@@ -0,0 +1,120 @@
+//パッケージ名の宣言
+package work5_1;
+/**
+ * ジャンケンのプレイヤーを表すクラス
+ */
+/*
+ * クラス名:PlayerClass
+ * 概要  :プレイヤークラス
+ * 作成者:R.Sugihara
+ * 作成日:2023.06.26
+ */
+public class PlayerClass {
+	//ジャンケンの手を表す定数
+	//グー
+	public static final int STONE_NUMBER = 0;
+	//チョキ
+	public static final int SCISSORS_NUMBER = 1;
+	//パー
+	public static final int PAPER_NUMBER = 2;
+	
+	/*
+	 * プレイヤークラスの属性
+	 */
+	/** プレイヤーの名前  */
+	private String playerName;
+	
+	/** プレイヤーの勝った回数 */
+	private int winCount = 0;
+	
+	/** 与えられた戦略 */
+	private JankenTactics playerTactics;
+	
+	/*
+	 * 関数名：setTactics
+	 * 概要：プレイヤーに戦略を渡す
+	 * 引数：JankenTactics setTactics
+	 * 戻り値：なし
+	 * 作成者：R.Sugihara
+	 * 作成日：2023.06.26
+	 */
+	void setTactics(JankenTactics setTactics) {
+		//プレイヤーに戦略を渡す
+		playerTactics = setTactics;
+	}
+	
+	/*
+	 * 関数名：PlayerClass
+	 * 概要：コンストラクタ
+	 * 引数：String nameString
+	 * 戻り値：なし
+	 * 作成者：R.Sugihara
+	 * 作成日：2023.06.26
+	 */
+	public PlayerClass(String nameString) {
+		//フィールドに引数nameStringの文字列を代入する
+		playerName = nameString;
+	}
+	
+	/*
+	 * プレイヤークラスの操作
+	 */
+	/*
+	 * 関数名：showHand
+	 * 概要：ジャンケンの手を出す
+	 * 引数：なし
+	 * 戻り値：playerHand
+	 * 作成者：R.Sugihara
+	 * 作成日：2023.06.26
+	 */
+	public int showHand() {
+		// 与えられた戦略を読んでジャンケンの手を決める
+		int playerHand = playerTactics.readTactics();
+		
+		// プレイヤーの出した手を返却する
+		return playerHand;
+	}
+	
+	/*
+	 * 関数名：notifyResult
+	 * 概要：審判から勝敗を聞く。true = 勝ち、false = 負け
+	 * 引数：boolean resultNumber
+	 * 戻り値：なし
+	 * 作成者：R.Sugihara
+	 * 作成日：2023.06.26
+	 */
+	public void notifyResult(boolean resultNumber) {
+		// 勝った場合
+		if (resultNumber == true) {
+			// 勝った回数を増やす
+			winCount++;
+		}
+	}
+	
+	/*
+	 * 関数名：getWinCount
+	 * 概要：自分の勝った回数を答える
+	 * 引数：なし
+	 * 戻り値：winCount
+	 * 作成者：R.Sugihara
+	 * 作成日：2023.06.26
+	 */
+	public int getWinCount() {
+		// 勝った回数を返却する
+		return winCount;
+	}
+	
+	/*
+	 * 関数名：getName
+	 * 概要：プレイヤーの名前を返却する
+	 * 引数：なし
+	 * 戻り値：playerName
+	 * 作成者：R.Sugihara
+	 * 作成日：2023.06.26
+	 */
+	public String getName() {
+		// プレイヤーの名前を返却する
+		return playerName;
+	}
+
+}
